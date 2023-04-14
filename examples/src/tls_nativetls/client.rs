6fbaa117160a7d0b2bfce38b375c3bde7e9793a2
Dawid Nowak <nowakd@gmail.com>
2023-04-14 19:20:27 +0100

    Working TCP

--- examples/src/tls_nativetls/client.rs
+++ examples/src/tls_nativetls/client.rs
@@ -30,34 +30,30 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut http = HttpConnector::new();
     http.enforce_http(false);
 
-
     // We have to do some wrapping here to map the request type from
     // `https://example.com` -> `https://[::1]:50051` because `rustls`
     // doesn't accept ip's as `ServerName`.
     let connector = tower::ServiceBuilder::new()
-        // .layer_fn(move |s| {
-        //     let tls = tls.clone();
+        .layer_fn(move |s| {
+            let tls = tls.clone();
 
-        //     hyper_rustls::HttpsConnectorBuilder::new()
-        //         .with_tls_config(tls)
-        //         .https_or_http()
-	// 	.enable_all_versions()
-        //         .wrap_connector(s)
-        // })
-        // // Since our cert is signed with `example.com` but we actually want to connect
-        // // to a local server we will override the Uri passed from the `HttpsConnector`
-        // // and map it to the correct `Uri` that will connect us directly to the local server.
-        // .map_request(|_| Uri::from_static("https://[::1]:50051"))
+            hyper_rustls::HttpsConnectorBuilder::new()
+                .with_tls_config(tls)
+                .https_or_http()
+                .enable_http2()
+                .wrap_connector(s)
+        })
+        // Since our cert is signed with `example.com` but we actually want to connect
+        // to a local server we will override the Uri passed from the `HttpsConnector`
+        // and map it to the correct `Uri` that will connect us directly to the local server.
+        .map_request(|_| Uri::from_static("https://[::1]:50051"))
         .service(http);
 
-    //    let client = hyper::Client::builder().build(connector);
-    let client = hyper::Client::builder().http2_only(true).build_http();
-
+    let client = hyper::Client::builder().build(connector);
 
     // Using `with_origin` will let the codegenerated client set the `scheme` and
     // `authority` from the porvided `Uri`.
-    //    let uri = Uri::from_static("https://example.com");
-    let uri = Uri::from_static("http://[::1]:50051");
+    let uri = Uri::from_static("https://example.com");
     let mut client = EchoClient::with_origin(client, uri);
 
     let request = tonic::Request::new(EchoRequest {
---
+++ examples/src/tls_nativetls/server.rs
@@ -0,0 +1,102 @@
+pub mod pb {
+    tonic::include_proto!("/grpc.examples.unaryecho");
+}
+
+use hyper::server::conn::Http;
+use pb::{EchoRequest, EchoResponse};
+use std::sync::Arc;
+use tokio::net::TcpListener;
+
+use tokio_native_tls::TlsAcceptor;
+use tokio_native_tls::TlsConnector;
+
+use tokio_native_tls::native_tls;
+use std::fs::File;
+use std::io::Read;
+
+use tonic::{transport::Server, Request, Response, Status};
+use tower_http::ServiceBuilderExt;
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let data_dir = std::path::PathBuf::from_iter([std::env!("CARGO_MANIFEST_DIR"), "data"]);
+    
+
+    let mut file = File::open(data_dir.join("tls/server.key")).unwrap();
+    let mut key = vec![];
+    file.read_to_end(&mut key).unwrap();
+
+    let mut file = File::open(data_dir.join("tls/server.pem")).unwrap();
+    let mut certs = vec![];
+    file.read_to_end(&mut certs).unwrap();
+    
+    let identity = native_tls::Identity::from_pkcs8(&certs,&key)?;
+    let tls_acceptor =  tokio_native_tls::TlsAcceptor::from(native_tls::TlsAcceptor::builder(identity).build()?);
+   
+
+    
+    let listener = TcpListener::bind("[::1]:50051").await?;
+    let server = EchoServer::default();
+
+    let svc = Server::builder()
+        .add_service(pb::echo_server::EchoServer::new(server))
+        .into_service();
+
+    let http = Http::new();
+    
+
+    loop{
+	let (socket, remote_addr) = listener.accept().await?;
+	let mut http = http.clone();
+     	http.http2_only(false);
+     	http.http1_only(false);
+	let svc = svc.clone();
+	println!("accept connection from {}", remote_addr);
+	
+	tokio::spawn(async move {
+            http.serve_connection(socket, svc).await.unwrap();
+        });    
+	
+    }
+    // loop {
+    //     // Asynchronously wait for an inbound socket.
+    //     let (socket, remote_addr) = listener.accept().await?;
+    //     let tls_acceptor = tls_acceptor.clone();
+	
+    // 	let http = http.clone();
+    // 	http.http2_only(false);
+    // 	http.http1_only(false);
+	
+    //     let svc = svc.clone();
+    //     println!("accept connection from {}", remote_addr);
+	
+    //     tokio::spawn(async move {
+    //         // Accept the TLS connection.
+    //         let tls_stream = tls_acceptor.accept(socket).await.expect("accept error");
+    //         // In a loop, read data from the socket and write the data back.
+    //         http.serve_connection(tls_stream, svc).await.unwrap();
+
+    //     });    
+    // }
+
+    
+}
+
+type EchoResult<T> = Result<Response<T>, Status>;
+
+#[derive(Default)]
+pub struct EchoServer;
+
+#[tonic::async_trait]
+impl pb::echo_server::Echo for EchoServer {
+    async fn unary_echo(&self, request: Request<EchoRequest>) -> EchoResult<EchoResponse> {
+        
+        println!(
+            "Got a request!"
+        );
+
+        let message = request.into_inner().message;
+        Ok(Response::new(EchoResponse { message }))
+    }
+}
