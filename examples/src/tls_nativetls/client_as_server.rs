6ac18f6a2bac00bcdc420ee8d283be69aefb57df
Dawid Nowak <nowakd@gmail.com>
2023-04-25 11:26:11 +0100

    Added streaming interface and running client in parallel

--- examples/src/tls_nativetls/client_as_server.rs
+++ examples/src/tls_nativetls/client_as_server.rs
@@ -2,20 +2,33 @@
 //! provide a custom `ClientConfig` for the tls configuration.
 
 pub mod pb {
-    tonic::include_proto!("/grpc.examples.unaryecho");
+    tonic::include_proto!("/grpc.examples.echo");
 }
 
+use futures::Stream;
 use std::iter::FromIterator;
 use pb::{EchoRequest,EchoResponse};
 use tokio_rustls::rustls::{ClientConfig, RootCertStore};
 use std::io::Read;
 use tokio::net::TcpStream;
-use tonic::{transport::Server, Request, Response, Status};
+use tonic::{transport::Server, Request, Response, Status,Streaming};
 use hyper::server::conn::Http;
+use std::{error::Error, io::ErrorKind, pin::Pin};
+use tokio_stream::{wrappers::ReceiverStream, StreamExt};
+use tokio::sync::mpsc;
 
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+type EchoResult<T> = Result<Response<T>, Status>;
+type ResponseStream = Pin<Box<dyn Stream<Item = Result<EchoResponse, Status>> + Send>>;
+
+
+
+//#[tokio::main]
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+    let rt = tokio::runtime::Runtime::new().unwrap();
     let data_dir = std::path::PathBuf::from_iter([std::env!("CARGO_MANIFEST_DIR"), "data"]);
     let mut fd = std::fs::File::open(data_dir.join("tls/ca.pem"))?;
 
@@ -32,19 +45,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 							     .danger_accept_invalid_certs(true)
 							     .request_alpns(&["h2"]).build().unwrap());
     
-
-    let stream = TcpStream::connect("[::1]:50051").await?;
-    let tls_stream = tls_connector.connect("http://[::1]:50051",stream).await?;
+    
+    let stream = rt.block_on(TcpStream::connect("[::1]:50051"))?;//.await
+    let tls_stream = rt.block_on(tls_connector.connect("http://[::1]:50051",stream))?;//.await?;
 
     let server = EchoServer::default();
     let svc = Server::builder()
         .add_service(pb::echo_server::EchoServer::new(server))
         .into_service();
-    http.serve_connection(tls_stream, svc).await.unwrap();            
+    rt.block_on(http.serve_connection(tls_stream, svc))?;
     Ok(())
 }
 
-type EchoResult<T> = Result<Response<T>, Status>;
 
 #[derive(Default)]
 pub struct EchoServer;
@@ -52,12 +64,98 @@ pub struct EchoServer;
 #[tonic::async_trait]
 impl pb::echo_server::Echo for EchoServer {
     async fn unary_echo(&self, request: Request<EchoRequest>) -> EchoResult<EchoResponse> {
-        
-        println!(
-            "Got a request!"
-        );
+        let request = request.into_inner();
+	tracing::info!(message = format!("EchoServer::unary_echo {:#?}", request));
+        Ok(Response::new(EchoResponse { message: request.message }))
+    }
 
-        let message = request.into_inner().message;
-        Ok(Response::new(EchoResponse { message }))
+    type ServerStreamingEchoStream = ResponseStream;
+    
+    async fn server_streaming_echo(
+        &self,
+        _req: Request<EchoRequest>,
+    ) -> EchoResult<Self::ServerStreamingEchoStream> {
+	Err(Status::unimplemented("not implemented"))
+    }
+
+    async fn client_streaming_echo(
+        &self,
+        _: Request<Streaming<EchoRequest>>,
+    ) -> EchoResult<EchoResponse> {
+        Err(Status::unimplemented("not implemented"))
+    }
+
+    type BidirectionalStreamingEchoStream = ResponseStream;
+
+    async fn bidirectional_streaming_echo(
+        &self,
+        req: Request<Streaming<EchoRequest>>,
+    ) -> EchoResult<Self::BidirectionalStreamingEchoStream> {
+	tracing::info!(message = "EchoServer::bidirectional_streaming_echo");
+
+
+        let mut in_stream = req.into_inner();
+        let (tx, rx) = mpsc::channel(128);
+
+        // this spawn here is required if you want to handle connection error.
+        // If we just map `in_stream` and write it back as `out_stream` the `out_stream`
+        // will be drooped when connection error occurs and error will never be propagated
+        // to mapped version of `in_stream`.
+        tokio::spawn(async move {
+            while let Some(result) = in_stream.next().await {
+                match result {
+                    Ok(v) => tx
+                        .send(Ok(EchoResponse { message: v.message }))
+                        .await
+                        .expect("working rx"),
+                    Err(err) => {
+                        if let Some(io_err) = match_for_io_error(&err) {
+                            if io_err.kind() == ErrorKind::BrokenPipe {
+                                // here you can handle special case when client
+                                // disconnected in unexpected way
+                                eprintln!("\tclient disconnected: broken pipe");
+                                break;
+                            }
+                        }
+
+                        match tx.send(Err(err)).await {
+                            Ok(_) => (),
+                            Err(_err) => break, // response was droped
+                        }
+                    }
+                }
+            }
+            println!("\tstream ended");
+        });
+
+        // echo just write the same data that was received
+        let out_stream = ReceiverStream::new(rx);
+
+        Ok(Response::new(
+            Box::pin(out_stream) as Self::BidirectionalStreamingEchoStream
+        ))
+    }
+}
+
+fn match_for_io_error(err_status: &Status) -> Option<&std::io::Error> {
+    let mut err: &(dyn Error + 'static) = err_status;
+
+    loop {
+        if let Some(io_err) = err.downcast_ref::<std::io::Error>() {
+            return Some(io_err);
+        }
+
+        // h2::Error do not expose std::io::Error with `source()`
+        // https://github.com/hyperium/h2/pull/462
+        if let Some(h2_err) = err.downcast_ref::<h2::Error>() {
+            if let Some(io_err) = h2_err.get_io() {
+                return Some(io_err);
+            }
+        }
+
+        err = match err.source() {
+            Some(err) => err,
+            None => return None,
+        };
     }
 }
--- examples/src/tls_nativetls/server_as_client.rs
+++ examples/src/tls_nativetls/server_as_client.rs
@@ -1,21 +1,21 @@
 pub mod pb {
-    tonic::include_proto!("/grpc.examples.unaryecho");
+    tonic::include_proto!("grpc.examples.echo");
 }
 
 use hyper::server::conn::Http;
 use tokio::net::TcpListener;
-use pb::{echo_client::EchoClient, EchoRequest,EchoResponse};
+use pb::{echo_client::EchoClient, EchoRequest};
 
 use tokio_native_tls::native_tls;
 use std::fs::File;
 use std::io::Read;
 
-use tonic::{Request, Response, Status};
 use tower::service_fn;
 use tonic::transport::{Endpoint, Uri};
 use tokio::sync::Mutex;
 use std::sync::Arc;
 use std::pin::Pin;
+use tonic::transport::Channel;
 
 use core::task::Context;
 use core::task::Poll;
@@ -25,6 +25,9 @@ use futures::{future};
 use std::io;
 use tokio::io::ReadBuf;
 use crate::io::Error;
+use futures::stream::Stream;
+use tokio_stream::StreamExt;
+use tokio::join;
 
 
 #[derive(Clone)]
@@ -102,9 +105,40 @@ impl tokio::io::AsyncWrite for MyConnector {
     
 }
 
+fn echo_requests_iter() -> impl Stream<Item = EchoRequest> {
+    tokio_stream::iter(1..usize::MAX).map(|i| EchoRequest {
+        message: format!("msg {:02}", i),
+    })
+}
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn bidirectional_streaming_echo( client:  &mut EchoClient<Channel>, num: usize) {
+    tracing::info!(message = "bidirectional_streaming_echo" );
+    let in_stream = echo_requests_iter().take(num);
+
+    let response = client
+        .bidirectional_streaming_echo(in_stream)
+        .await
+        .unwrap();
+
+    let mut resp_stream = response.into_inner();
+
+    while let Some(received) = resp_stream.next().await {
+        let received = received.unwrap();
+	let msg = format!("received message {:#?}", received.message);
+	tracing::info!(message = "Got message ", %msg);
+        
+    }
+    tracing::info!(message = "bidirectional_streaming_echo done");
+}
+
+
+//#[tokio::main]
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::FmtSubscriber::builder()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+    
+    let rt = tokio::runtime::Runtime::new().unwrap();
     let data_dir = std::path::PathBuf::from_iter([std::env!("CARGO_MANIFEST_DIR"), "data"]);
     
 
@@ -120,59 +154,66 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let tls_acceptor = tokio_native_tls::TlsAcceptor::from(native_tls::TlsAcceptor::builder(identity).build()?);
     
         
-    let listener = TcpListener::bind("[::1]:50051").await?;
+    //let listener = TcpListener::bind("[::1]:50051").await?;
+    let listener = rt.block_on(TcpListener::bind("[::1]:50051"))?;
     let http = Http::new();    
     
     
     loop {
         // Asynchronously wait for an inbound socket.
-        let (socket, remote_addr) = listener.accept().await?;
+	//let (socket, remote_addr) = listener.accept().await?;
+        let (socket, remote_addr) = rt.block_on(listener.accept())?;
         let tls_acceptor = tls_acceptor.clone();	
 	let mut http = http.clone();
 	http.http2_only(true);
-			    
-        println!("accept connection from {}", remote_addr);
+
+
+	tracing::info!(message = "Accept connection from .", %remote_addr);
+        
 	
-        tokio::spawn(async move  {
+        rt.spawn(async move  {
             // Accept the TLS connection.
 	    let tls_stream = tls_acceptor.accept(socket).await.expect("accept error");
 
 	    let connector = MyConnector{
 		inner: Arc::new(Mutex::new(tls_stream))
 	    };
-	   
+
+
 	    let channel = Endpoint::try_from("http://[::1]:50051").unwrap()
 		.connect_with_connector(service_fn(move |_: Uri| {
 		    let connector = connector.clone();
-
+		    
 		    future::ready(Ok::<MyConnector, io::Error>(connector))
-            })).await.unwrap();
-	                	    
-	    let mut client = EchoClient::new(channel);
-	    let request = tonic::Request::new(EchoRequest {
-		message: "hello".into(),
+		})).await.unwrap();
+	    
+	    let client = EchoClient::new(channel);
+	    let client = Arc::new(Mutex::new(client));
+	    let unary_client =  client.clone();
+	    let stream_client =  client.clone();
+	    
+	    let unary_task = tokio::spawn( async move  {		
+		let request = tonic::Request::new(EchoRequest {
+		    message: "hello".into(),
+		});
+		let client = unary_client;
+		let mut client = client.lock().await;
+		tracing::info!(message = format!("Unary request {:?}", request));
+		let response = client.unary_echo(request).await.unwrap();
+		tracing::info!(message = format!("Unary response {:?}", response));
 	    });
 
-	    let response = client.unary_echo(request).await.unwrap();
-	    println!("RESPONSE={:?}", response);	    
-        });    
+	    let stream_task = tokio::spawn( async move  {
+		let client = stream_client;
+		let mut client = client.lock().await;
+		bidirectional_streaming_echo(&mut *client, 100).await;
+	    });
+
+	    let res = join!(stream_task,unary_task);
+	    tracing::info!(message = format!("gRPC client done {:?}", res));
+        });
+	
     }    
 }
 
-type EchoResult<T> = Result<Response<T>, Status>;
 
-#[derive(Default)]
-pub struct EchoServer;
-
-#[tonic::async_trait]
-impl pb::echo_server::Echo for EchoServer {
-    async fn unary_echo(&self, request: Request<EchoRequest>) -> EchoResult<EchoResponse> {
-        
-        println!(
-            "Got a request!"
-        );
-
-        let message = request.into_inner().message;
-        Ok(Response::new(EchoResponse { message }))
-    }
-}
